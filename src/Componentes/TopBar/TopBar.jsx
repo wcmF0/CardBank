57c8809d791be85b47c6b5f2f32ea2dfdd8a80ca
wcmF0 <wagnerfilho696@gmail.com>
2025-03-20 15:24:49 -0300

    Enhance responsive design: update TopBar and MainContent styles for better mobile experience

--- src/Componentes/TopBar/TopBar.jsx
+++ src/Componentes/TopBar/TopBar.jsx
@@ -9,10 +9,10 @@ const TopBar = () => {
         <li className="hover-effect">
           <img className="img" src={CardBankLogo} alt="Logo do CardBank" />
         </li>
-        <li className="hover-effect">Para você</li>
-        <li className="hover-effect">Para empresas</li>
-        <li className="hover-effect">Ajuda</li>
-        <li className="hover-effect">Busca</li>
+        <li className="hover-effect para-voce">Para você</li>
+        <li className="hover-effect empresas">Para empresas</li>
+        <li className="hover-effect ajuda">Ajuda</li>
+        <li className="hover-effect busca">Busca</li>
         <li>
           <div className="AgenciaConta">
             <input type="text" maxLength={4} placeholder="Agência" />
@@ -20,7 +20,7 @@ const TopBar = () => {
             <input type="text" maxLength={6} placeholder="Conta" />
           </div>
         </li>
-        <button>
+        <button className="botao-buscar">
           <img src={ArrowRigth} alt="" />
         </button>
         <div className="AbraConta">
